74c813d810da9613c6b060a8af35097c85b72af2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-14 20:01:35 -0300

    feat: ex 3 and 4

--- src/components/Ex3.jsx
+++ src/components/Ex3.jsx
@@ -6,7 +6,12 @@ export default function Ex3() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex3 - Grid Simples</h1>
-      {/* Resolva aqui */}
+        <div className="grid grid-cols-2 grid-rows-2 gap-2">
+          <div className="h-[80px] bg-gray-600 col-span-1 row-span-1 "></div>
+          <div className="h-[80px] bg-gray-600 col-span-1 row-span-1 "></div>
+          <div className="h-[80px] bg-gray-600 col-span-1 row-span-1 "></div>
+          <div className="h-[80px] bg-gray-600 col-span-1 row-span-1  "></div>
+        </div>
     </div>
   );
 }
--- src/components/Ex4.jsx
+++ src/components/Ex4.jsx
@@ -5,8 +5,17 @@
 export default function Ex4() {
   return (
     <div className="my-8">
-        <h1 className="text-2xl font-bold mb-4">Ex4 - Card Responsivo</h1>
-      {/* Resolva aqui */}
+      <h1 className="text-2xl font-bold mb-4">Ex4 - Card Responsivo</h1>
+      <div className="mx-2 md:w-96 bg-gray-200 shadow-lg rounded-xl min-h-[200px] h-auto p-2 flex justify-between flex-col items-center">
+        <h1 className="text-xl font-bold">Title</h1>
+        <p className="text-[14px] ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ante
+          dui, sollicitudin id enim vel, blandit ullamcorper libero. Orci varius
+          natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+          mus. Pellentesque{" "}
+        </p>
+        <button className="w-[50%] bg-gray-300">add </button>
+      </div>
     </div>
   );
 }
